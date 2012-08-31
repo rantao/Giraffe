9dbff41e8a8eab4a64b6eeec7e13416e0b7f8398
Ran Tao <ran@ephexi.com>
2012-08-31 14:20:39 -0700

    Initial Commit

---
+++ Giraffe/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Giraffe
+//
+//  Created by Ran Tao on 8.31.12.
+//  Copyright (c) 2012 Ran Tao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
